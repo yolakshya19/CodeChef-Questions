1385d3eb04c1a5b493acff277635040102da2b8c
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-25 23:47:22 +0530

    Create Important-Pages.cpp

---
+++ Important-Pages.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin >> a >> b;
+	if(a==0){
+	    cout << "https://www.codechef.com/practice" << endl;
+	}
+	else if(a==1 && b==0){
+	    cout << "https://www.codechef.com/contests" << endl;
+	}
+	else if(a==1 && b==1){
+	    cout << "https://discuss.codechef.com" << endl;
+	}
+	return 0;
+}
