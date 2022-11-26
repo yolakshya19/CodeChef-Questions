5905560840e8b4f2eed05dca5be1b2d4fa7a81ab
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-26 19:45:24 +0530

    Create sum-difference.cpp

---
+++ sum-difference.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n1,n2;
+	cin >> n1 >> n2;
+	if(n1>n2){
+	    cout << n1-n2 << endl;
+	}
+	else{
+	    cout << n1+n2 << endl;
+	}
+	return 0;
+}
