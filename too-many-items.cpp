147d8426c2912a27f5c1d4ca75e4ccbc1abcc601
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-26 16:00:22 +0530

    Create too-many-items.cpp

---
+++ too-many-items.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << ceil(n/10.0) << endl;
+	}
+	return 0;
+}
