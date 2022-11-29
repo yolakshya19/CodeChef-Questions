cd65791392e769079bc506a56fe5de03ee5c8be3
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-29 21:21:57 +0530

    Create paper-cutting.cpp

---
+++ paper-cutting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,k;
+	    cin >> n >> k;
+	    cout << (n/k)*(n/k) << endl;
+	}
+	return 0;
+}
