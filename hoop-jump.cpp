3353e3d3544911a2fa478cc0c0c8bd1a2a1d0752
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-29 20:40:19 +0530

    Create hoop-jump.cpp

---
+++ hoop-jump.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << ceil(n/2.0) << endl;
+	}
+	return 0;
+}
