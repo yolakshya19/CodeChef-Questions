50063b3b8142b03a0f9bd04250fc2c829e13221b
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-29 20:43:17 +0530

    Create helping-chef.cpp

---
+++ helping-chef.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    if(n<10){
+	        cout << "Thanks for helping Chef!" << endl;
+	    }
+	    else{
+	        cout << -1 << endl;
+	    }
+	}
+	return 0;
+}
