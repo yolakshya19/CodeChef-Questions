4a094792221b4f9fcd84f2522eb008a6bd35080b
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-29 20:40:59 +0530

    Create richie-rich.cpp

---
+++ richie-rich.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b,x;
+	    cin >> a >> b >> x;
+	    cout << (b-a)/x << endl;
+	}
+	return 0;
+}
