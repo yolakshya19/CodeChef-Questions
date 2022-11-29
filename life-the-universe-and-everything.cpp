a1788d1e9528964cfa9a05465a001e71464190e4
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-11-29 20:57:08 +0530

    Create life-the-universe-and-everything.cpp

---
+++ life-the-universe-and-everything.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	while(cin >> n){
+	    if(n==42){
+	        break;
+	    }
+	    cout << n << endl;
+	}
+	return 0;
+}
