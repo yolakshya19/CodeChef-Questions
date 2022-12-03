6462434ee5e23a6c6246f0cf945dcda636e92495
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-03 13:31:36 +0530

    Create badminton-serves.cpp

---
+++ badminton-serves.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int p;
+	    cin >> p;
+	    cout << (p/2)+1 << endl;
+	}
+	return 0;
+}
