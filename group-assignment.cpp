df6d7803678d0a5802e908b299b0d53fc77d3bde
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-03 14:16:35 +0530

    Create group-assignment.cpp

---
+++ group-assignment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,x;
+	    cin >> n >> x;
+	    cout << 2*n-(x-1) << endl;
+	}
+	return 0;
+}
