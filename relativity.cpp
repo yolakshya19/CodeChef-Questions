08c1b6a4804fa961bd74a013358ffeb0640f8257
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-03 13:38:23 +0530

    Create relativity.cpp

---
+++ relativity.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int g,c;
+	    cin >> g >> c;
+	    cout << (c*c)/(2*g) << endl;
+	}
+	return 0;
+}
