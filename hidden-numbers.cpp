d9dc66b354c47897c8b80a1d30567dd7c71be4bf
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-04 14:14:34 +0530

    Create hidden-numbers.cpp

---
+++ hidden-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << 1 << " " << n << endl;
+	}
+	return 0;
+}
