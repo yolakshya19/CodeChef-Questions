bbd7e2c799886bd9bcabd72a84c95f0d91cfb859
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-04 14:13:28 +0530

    Create the-rating-dilemma.cpp

---
+++ the-rating-dilemma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int s;
+	    cin >> s;
+	    int x=s+1;
+	    int y=s-x;
+	    cout << x*y << endl;
+	}
+	return 0;
+}
