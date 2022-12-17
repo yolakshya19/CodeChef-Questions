96ae3cc918d94ca2eccb66e9f990d1ec10c7b338
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-17 14:02:35 +0530

    Create avoid-square-places.cpp

---
+++ avoid-square-places.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    for(int i=1;i<=n;i++){
+	        cout << i << " " ;
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
