f0bd9540f6ff5a6e9eed9026b0163e7f8582bf62
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-20 20:52:13 +0530

    Create balls-and-boxes.cpp

---
+++ balls-and-boxes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,k;
+	    cin >> n >> k;
+	    int balls=0;
+	    while(k>0){
+	        balls+=b;
+	        k--;
+	    }
+	    if(n>=balls) cout << "Yes" << endl;
+	    else cout << "No" << endl;
+	}
+	return 0;
+}
