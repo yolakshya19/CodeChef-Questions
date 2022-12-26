26fc455dd1077a81adfe4464036cc64d3d7a9f68
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-26 20:48:57 +0530

    Create packaging-cupcakes.cpp

---
+++ packaging-cupcakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << n/2+1 << endl;
+	}
+	return 0;
+}
