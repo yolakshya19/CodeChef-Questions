05310d68023fe97b20f8676197c78fac87c5e30a
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-12-28 21:15:52 +0530

    Create perfect-trio.cpp

---
+++ perfect-trio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b,c;
+	    cin >> a >> b >> c;
+	    if(a==b+c || b==a+c || c==a+b) cout << "YES" << endl;
+	    else cout << "NO" << endl;
+	}
+	return 0;
+}
