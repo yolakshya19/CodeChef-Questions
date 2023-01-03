9fb5389a0ed2b907914e979765f4890a2af715b2
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2023-01-03 18:54:35 +0530

    Create the-one-where-it-all-began.cpp

---
+++ the-one-where-it-all-began.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    if(n>20) cout << "YES" << endl;
+	    else cout << "NO" << endl;
+	}
+	return 0;
+}
