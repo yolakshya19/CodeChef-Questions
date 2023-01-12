b02580f49e8a428fb79e6d31f40906412fd95109
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2023-01-12 13:37:37 +0530

    Create chef-plays-ludo.cpp

---
+++ Starter-73/chef-plays-ludo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(x==6) cout << "Yes" << endl;
+	    else cout << "No" << endl;
+	}
+	return 0;
+}
